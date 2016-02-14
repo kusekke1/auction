cbbdeb753482d134c78b02d1c4ea6a813de99d4b
tomoki <kusekke1@gmail.com>
2016-02-14 18:41:25 +0900

    編集機能までを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,34 @@ class ItemsController < ApplicationController
     def index
         @items = Item.all
     end
+
     def show
         @item = Item.find(params[:id])
     end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        @item = Item.new(item_params)
+        @item.save #うえで入れた情報をsaveメソッドで処理 DBに登録される
+        redirect_to @item #createはviewがないのでリダイレクト処理を行う
+    end
+
+    def edit
+        @item = Item.find(params[:id])
+    end
+
+    def update
+        @item = Item.find(params[:id]) #更新かける前に情報を取得
+        @item.update(item_params) #25行目で狙いを定めて、updateをかけるよ、内容はitem_paramsだよ
+        redirect_to @item
+    end
+
+    private
+    def item_params
+        params.require(:item).permit(:name, :description, :price, :seller_id, :email, :image_url)
+    end
+
 end
